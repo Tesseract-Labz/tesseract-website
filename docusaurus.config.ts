37dd43d023e2228e4e4ca2610568500603800f89
frollo <flagging out>
2026-01-02 21:30:24 +0100

    Add info email as CTA in the navbar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -89,8 +89,8 @@ const config: Config = {
         },
         { to: "/blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/Tesseract-Labz/",
-          label: "GitHub",
+          href: "mailto:info@tesseractlabz.com",
+          label: "Get in Touch",
           position: "right",
         },
       ],
